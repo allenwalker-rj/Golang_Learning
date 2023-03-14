417d9f58f983362524184239c2a7d2386b9a51f0
allenwalker-rj <734403439@qq.com>
2023-03-14 10:31:14 +0800

    🏷️ add if condition

--- main/service-4.go
+++ main/service-4.go
@@ -13,7 +13,23 @@ func main() {
 	}
 
 	// 在Golang里，if后面可以并列的加入变量的定义
-	if count := 20; count < 30 {
+	if age2 := 45; age2 <= 35 {
+		fmt.Println("年龄满足条件")
+	} else {
 		fmt.Println("年龄不满足条件")
+	}
+
+	if count := 80; count >= 100 {
+		fmt.Println("你的成绩等级为: S")
+	} else if count >= 90 {
+		fmt.Println("你的成绩等级为: A")
+	} else if count >= 80 {
+		fmt.Println("你的成绩等级为: B")
+	} else if count >= 70 {
+		fmt.Println("你的成绩等级为: C")
+	} else if count >= 60 {
+		fmt.Println("你的成绩等级为: D")
+	} else {
+		fmt.Println("你的成绩等级为: 天才")
 	}
 }
